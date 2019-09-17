16ce38078d9d088d2be33a35887a7392412a653b
Ernesto <02ernesto.rodriguez@gmail.com>
2019-09-17 18:13:18 -0500

    semiterminado

--- test/js/main.js
+++ test/js/main.js
@@ -1,11 +1,75 @@
 //new array
+const rym = [
+    
+    {
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male"
+    },
+
+   { name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male"},
+
+    {name: "Summer Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Female"},
+
+    {name: "Beth Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Female"},
+
+   { name: "Jerry Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male"},
+
+    {name: "Abadango Cluster Princess",
+    status: "Alive",
+    species: "Alien",
+    type: "",
+    gender: "Female"},
+
+   { name: "Abradolf Lincler",
+    status: "unknown",
+    species: "Human",
+    type: "Genetic experiment",
+    gender: "Male"},
+
+    {name: "Adjudicator Rick",
+    status: "Dead",
+    species: "Human",
+    type: "",
+    gender: "Male"},
+
+    {name: "Agency Director",
+    status: "Dead",
+    species: "Human",
+    type: "",
+    gender: "Male"},
+
+    {name: "Alan Rails",
+    status: "Dead",
+    species: "Human",
+    type: "Superhuman (Ghost trains summoner)",
+    gender: "Male"}
+]
 
 
 //Get the first three elements of the Array without altering the original Array
 getFirtsThreeElements=()=>{
     //...write your code here
-
-    console.log('The first three',/** here your new answer*/)
+    var rick = rym.slice(0,3);
+    console.log('The first three',+rick);
 }
 //Find all the characters whose species is Aliens nad return in new array
 getAliens=()=>{
@@ -15,8 +79,14 @@ getAliens=()=>{
 //create a new character and add it to the matrix and add consecutive identifiers (1,2, ... n) to all characters
 newCharacter=()=>{
     //...write your code here
+    var nuevo = {name: "Nuevo",
+    status: "Dead",
+    species: "Human",
+    type: "Reptiliano",
+    gender: "Male"}
 
-    console.log('New Charanter and id to all:',/** here your new answer*/)
+    rick.push(nuevo);
+    console.log('New Charanter and id to all:'+ rick)
 }
 //find the family smith (Morty, Beth, Summer, Jerry, Rick)  in a new Array called familySmith
 getFamilySmith=()=>{
@@ -27,7 +97,7 @@ getFamilySmith=()=>{
 //Jerry and Beth have divorced, remove Jerry from the Array Smith Family, and get a random family member and print all their values
 familyBroken=()=>{
     //...write your code here
-
+    
     console.log('Family without Jerry',/** here your new answer*/)
     console.log('Random member name:, status:, species:, type:, gender:')
 }
